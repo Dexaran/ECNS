18dd579595f8c6a630132911edfce76781204a53
Dexaran <dexaran820@gmail.com>
2017-05-27 09:11:54 +0400

    Update PublicResolver.sol

--- PublicResolver.sol
+++ PublicResolver.sol
@@ -1,18 +1,46 @@
 pragma solidity ^0.4.0;
 
-import 'interface.sol';
+import './AbstractENS.sol';
 
 /**
  * A simple resolver anyone can use; only allows the owner of a node to set its
  * address.
  */
 contract PublicResolver {
+    bytes4 constant INTERFACE_META_ID = 0x01ffc9a7;
+    bytes4 constant ADDR_INTERFACE_ID = 0x3b3b57de;
+    bytes4 constant CONTENT_INTERFACE_ID = 0xd8389dc5;
+    bytes4 constant NAME_INTERFACE_ID = 0x691f3431;
+    bytes4 constant ABI_INTERFACE_ID = 0x2203ab56;
+    bytes4 constant PUBKEY_INTERFACE_ID = 0xc8690233;
+    bytes4 constant TEXT_INTERFACE_ID = 0x59d1d43c;
+
+    event AddrChanged(bytes32 indexed node, address a);
+    event ContentChanged(bytes32 indexed node, bytes32 hash);
+    event NameChanged(bytes32 indexed node, string name);
+    event ABIChanged(bytes32 indexed node, uint256 indexed contentType);
+    event PubkeyChanged(bytes32 indexed node, bytes32 x, bytes32 y);
+    event TextChanged(bytes32 indexed node, string indexed indexedKey, string key);
+
+    struct PublicKey {
+        bytes32 x;
+        bytes32 y;
+    }
+
+    struct Record {
+        address addr;
+        bytes32 content;
+        string name;
+        PublicKey pubkey;
+        mapping(string=>string) text;
+        mapping(uint256=>bytes) abis;
+    }
+
     AbstractENS ens;
-    mapping(bytes32=>address) addresses;
-    mapping(bytes32=>bytes32) hashes;
+    mapping(bytes32=>Record) records;
 
     modifier only_owner(bytes32 node) {
-        if(ens.owner(node) != msg.sender) throw;
+        if (ens.owner(node) != msg.sender) throw;
         _;
     }
 
@@ -25,39 +53,27 @@ contract PublicResolver {
     }
 
     /**
-     * Fallback function.
-     */
-    function() {
-        throw;
-    }
-
-    /**
-     * Returns true if the specified node has the specified record type.
-     * @param node The ENS node to query.
-     * @param kind The record type name, as specified in EIP137.
-     * @return True if this resolver has a record of the provided type on the
-     *         provided node.
-     */
-    function has(bytes32 node, bytes32 kind) constant returns (bool) {
-        return (kind == "addr" && addresses[node] != 0) || (kind == "hash" && hashes[node] != 0);
-    }
-    
-    /**
      * Returns true if the resolver implements the interface specified by the provided hash.
      * @param interfaceID The ID of the interface to check for.
      * @return True if the contract implements the requested interface.
      */
     function supportsInterface(bytes4 interfaceID) constant returns (bool) {
-        return interfaceID == 0x3b3b57de || interfaceID == 0xd8389dc5;
+        return interfaceID == ADDR_INTERFACE_ID ||
+               interfaceID == CONTENT_INTERFACE_ID ||
+               interfaceID == NAME_INTERFACE_ID ||
+               interfaceID == ABI_INTERFACE_ID ||
+               interfaceID == PUBKEY_INTERFACE_ID ||
+               interfaceID == TEXT_INTERFACE_ID ||
+               interfaceID == INTERFACE_META_ID;
     }
-    
+
     /**
      * Returns the address associated with an ENS node.
      * @param node The ENS node to query.
      * @return The associated address.
      */
     function addr(bytes32 node) constant returns (address ret) {
-        ret = addresses[node];
+        ret = records[node].addr;
     }
 
     /**
@@ -67,9 +83,10 @@ contract PublicResolver {
      * @param addr The address to set.
      */
     function setAddr(bytes32 node, address addr) only_owner(node) {
-        addresses[node] = addr;
+        records[node].addr = addr;
+        AddrChanged(node, addr);
     }
-    
+
     /**
      * Returns the content hash associated with an ENS node.
      * Note that this resource type is not standardized, and will likely change
@@ -78,9 +95,9 @@ contract PublicResolver {
      * @return The associated content hash.
      */
     function content(bytes32 node) constant returns (bytes32 ret) {
-        ret = hashes[node];
+        ret = records[node].content;
     }
-    
+
     /**
      * Sets the content hash associated with an ENS node.
      * May only be called by the owner of that node in the ENS registry.
@@ -90,6 +107,106 @@ contract PublicResolver {
      * @param hash The content hash to set
      */
     function setContent(bytes32 node, bytes32 hash) only_owner(node) {
-        hashes[node] = hash;
+        records[node].content = hash;
+        ContentChanged(node, hash);
+    }
+
+    /**
+     * Returns the name associated with an ENS node, for reverse records.
+     * Defined in EIP181.
+     * @param node The ENS node to query.
+     * @return The associated name.
+     */
+    function name(bytes32 node) constant returns (string ret) {
+        ret = records[node].name;
+    }
+    
+    /**
+     * Sets the name associated with an ENS node, for reverse records.
+     * May only be called by the owner of that node in the ENS registry.
+     * @param node The node to update.
+     * @param name The name to set.
+     */
+    function setName(bytes32 node, string name) only_owner(node) {
+        records[node].name = name;
+        NameChanged(node, name);
+    }
+
+    /**
+     * Returns the ABI associated with an ENS node.
+     * Defined in EIP205.
+     * @param node The ENS node to query
+     * @param contentTypes A bitwise OR of the ABI formats accepted by the caller.
+     * @return contentType The content type of the return value
+     * @return data The ABI data
+     */
+    function ABI(bytes32 node, uint256 contentTypes) constant returns (uint256 contentType, bytes data) {
+        var record = records[node];
+        for(contentType = 1; contentType <= contentTypes; contentType <<= 1) {
+            if ((contentType & contentTypes) != 0 && record.abis[contentType].length > 0) {
+                data = record.abis[contentType];
+                return;
+            }
+        }
+        contentType = 0;
+    }
+
+    /**
+     * Sets the ABI associated with an ENS node.
+     * Nodes may have one ABI of each content type. To remove an ABI, set it to
+     * the empty string.
+     * @param node The node to update.
+     * @param contentType The content type of the ABI
+     * @param data The ABI data.
+     */
+    function setABI(bytes32 node, uint256 contentType, bytes data) only_owner(node) {
+        // Content types must be powers of 2
+        if (((contentType - 1) & contentType) != 0) throw;
+        
+        records[node].abis[contentType] = data;
+        ABIChanged(node, contentType);
+    }
+    
+    /**
+     * Returns the SECP256k1 public key associated with an ENS node.
+     * Defined in EIP 619.
+     * @param node The ENS node to query
+     * @return x, y the X and Y coordinates of the curve point for the public key.
+     */
+    function pubkey(bytes32 node) constant returns (bytes32 x, bytes32 y) {
+        return (records[node].pubkey.x, records[node].pubkey.y);
+    }
+    
+    /**
+     * Sets the SECP256k1 public key associated with an ENS node.
+     * @param node The ENS node to query
+     * @param x the X coordinate of the curve point for the public key.
+     * @param y the Y coordinate of the curve point for the public key.
+     */
+    function setPubkey(bytes32 node, bytes32 x, bytes32 y) only_owner(node) {
+        records[node].pubkey = PublicKey(x, y);
+        PubkeyChanged(node, x, y);
+    }
+
+    /**
+     * Returns the text data associated with an ENS node and key.
+     * @param node The ENS node to query.
+     * @param key The text data key to query.
+     * @return The associated text data.
+     */
+    function text(bytes32 node, string key) constant returns (string ret) {
+        ret = records[node].text[key];
+    }
+
+    /**
+     * Sets the text data associated with an ENS node and key.
+     * May only be called by the owner of that node in the ENS registry.
+     * @param node The node to update.
+     * @param key The key to set.
+     * @param value The text data value to set.
+     */
+    function setText(bytes32 node, string key, string value) only_owner(node) {
+        records[node].text[key] = value;
+        TextChanged(node, key, key);
     }
 }
