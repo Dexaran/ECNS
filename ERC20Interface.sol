c46fe9d82253a9b5a7e926b28defa4ec10130a50
Dexaran <dexaran820@gmail.com>
2017-06-04 10:19:56 +0400

    Add files via upload

---
+++ ERC20Interface.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.9;
+
+/**
+ * The ECNS registry contract.
+ */
+contract ERC20Interface {
+    uint256 public totalSupply;
+    function balanceOf(address who) constant returns (uint256);
+    function allowance(address owner, address spender) constant returns (uint256);
+    function transfer(address to, uint256 value) returns (bool ok);
+    function transferFrom(address from, address to, uint256 value) returns (bool ok);
+    function approve(address spender, uint256 value) returns (bool ok);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
